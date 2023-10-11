6b434a3be666aae2803f2e8029cc240e9e836928
Jayden <sqwatato@gmail.com>
2023-10-10 20:22:43 -0700

    contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,29 +1,76 @@
 /**
  * Contact
  */
-
+import { FaMapMarkerAlt, FaPhone } from "react-icons/fa";
 export default function Contact() {
   return (
     <section className="bg-gray-900 custom-hidden">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md h-full pb-40">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Want to book an appointment? Have questions about HI-TECH Barber Shop? Let us know!</p>
-        <form action="#" className="space-y-8">
-          <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="example@gmail.com" required />
+        {/* <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+          Contact Us
+        </h2> */}
+        {/* <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          Want to book an appointment? Have questions about HI-TECH Barber Shop?
+          Let us know!
+        </p> */}
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="lg:text-center">
+            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+              Contact Us
+            </h2>
+            <p className="mb-4 text-4xl tracking-tight font-bold text-center text-gray-900 dark:text-white">
+              Get in touch
+            </p>
+            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+              Want to book an appointment? Have questions about HI-TECH Barber
+              Shop? Let us know!
+            </p>
           </div>
-          <div>
-            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-            <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you." required />
+
+          <div className="mt-10 flex flex-col sm:flex-row justify-center space-x-6">
+            <div className="flex flex-row items-center mb-4 sm:mb-0 sm:flex-row sm:items-start">
+              <div>
+                <div className="flex">
+                  <FaMapMarkerAlt className="h-6 w-6 text-indigo-600" />
+                  <span className="ml-2 text-white">
+                    123 Main St, Cupertino, CA 95014
+                  </span>
+                </div>
+                <br />
+                <div className="flex">
+                  <FaPhone className="h-6 w-6 text-indigo-600" />
+                  <span className="ml-2 text-white">(408) 555 - 5555</span>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <table className="table-auto">
+                <thead>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className=" px-4 py-2">Mon</td>
+                    <td className=" px-4 py-2">Closed</td>
+                  </tr>
+                  <tr>
+                    <td className=" px-4 py-2">Tue - Fri</td>
+                    <td className=" px-4 py-2">12:00 PM - 6:30 PM</td>
+                  </tr>
+                  <tr>
+                    <td className=" px-4 py-2">Sat</td>
+                    <td className=" px-4 py-2">12:00 PM - 6:00 PM</td>
+                  </tr>
+                  <tr>
+                    <td className=" px-4 py-2">Sun</td>
+                    <td className=" px-4 py-2">Closed</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-            <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..." />
-          </div>
-          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send Message</button>
-        </form>
+        </div>
       </div>
     </section>
-    )
+  );
 }
